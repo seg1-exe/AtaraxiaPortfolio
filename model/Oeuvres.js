8404293f6390b3f1ba7d0dc9b693da80724c9cc0
Rutabagarre <84787988+Rutabagarre@users.noreply.github.com>
2022-12-22 20:25:33 +0100

    avancement projet

--- model/Oeuvres.js
+++ model/Oeuvres.js
@@ -3,7 +3,11 @@ import {model, Schema} from 'mongoose';
 const oeuvreSchema = new Schema({
     nom : {type : String, required : true},
     file : {type : String, required : true},
-    description : {type : String, required: true}
+    description : {type : String, required: true},
+    work1: {type : String, required: false},
+    work2: {type : String, required: false},
+    work3: {type : String, required: false},
+    youtube: {type : String, required: false},
 });
 
 export default model('oeuvre', oeuvreSchema);
--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,18 @@ window.addEventListener('load', ()=> {
             nav.classList.remove('nav-scroll')
         }
     })
+
+    let copytext = document.querySelector(".copy-text")
+    copytext.querySelector("button").addEventListener("click", function(){
+        let input = copytext.querySelector("input.text");
+        input.select();
+        document.execCommand('copy');
+        copytext.classList.add('active');
+        window.getSelection.removeAllRanges();
+        setTimeout(function(){
+            copytext.classList.remove('active');
+        },2500);
+    });
 })
 
 
